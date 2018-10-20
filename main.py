fbaa0b7f256585b18fb34c7ffaaae8bcd74c2a0e
berk <bdehrioglu@yahoo.com>
2018-10-20 21:38:49 +0300

    feat:Prob Density Function

--- main.py
+++ main.py
@@ -13,28 +13,49 @@ def histogram(i):
     return hist
 
 
+def probdensityfunc(histogram, shape):
+    probdensity = np.zeros((256, 1, 3), dtype=np.uint8)
+    probdensity = histogram / shape
+    return probdensity
+
+
 histogram1 = np.zeros((256, 1, 3), dtype=np.uint8)
 histogram2 = np.zeros((256, 1, 3), dtype=np.uint8)
+probDensity1 = np.zeros((256, 1, 3), dtype=np.float32)
+probDensity2 = np.zeros((256, 1, 3), dtype=np.float32)
+cdf = np.zeros((256,1,1),dtype=np.uint8)
 
 x_pos = [i for i in range(256)]
 
-img = cv2.imread("color1.png")
-img2 = cv2.imread("color2.png")
+img = cv2.imread("color1.png", cv2.IMREAD_COLOR)
+img2 = cv2.imread("color2.png", cv2.IMREAD_COLOR)
+imgShape = img.shape[0] * img.shape[1]
 
 histogram1 = histogram(img)
 histogram2 = histogram(img2)
-print(img.shape)
-print(img2.shape)
-print(histogram1[...,0,0])
-print(histogram2.shape)
 
-plt.bar(x_pos,histogram1[...,0,2])
+probDensity1 = probdensityfunc(histogram1, imgShape)
+probDensity2 = probdensityfunc(histogram2, imgShape)
+
+
+print(probDensity1)
+print(probDensity2)
+
+plt.bar(x_pos, histogram1[..., 0, 1])
+plt.title("hist")
 plt.show()
+
+plt.bar(x_pos, probDensity1[..., 0, 1])
+plt.title("probdens")
+plt.show()
+
+
+
 
 cv2.imshow('img', img2)
 
 if cv2.waitKey(0)&0xFF == 27:         # wait for ESC key to exit
     cv2.destroyAllWindows()
 elif cv2.waitKey(0)&0xFF == ord('s'):  # wait for 's' key to save and exit
-    cv2.imwrite('color1.jpeg', img)
+    cv2.imwrite('color1.png', img)
     cv2.destroyAllWindows()
