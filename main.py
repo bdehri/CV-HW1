b84c0a569b49591dea60ebd91b13c7f2fb1ca2aa
berk <bdehrioglu@yahoo.com>
2018-10-20 22:45:58 +0300

    fix: No Histogram Value greater then 255 bug fix There was a bug such that no histogram value greater than 255 Instead of uint8, now uint32 is being used

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 def histogram(i):
     R, C, B = i.shape
-    hist = np.zeros((256,1,B),dtype=np.uint8)
+    hist = np.zeros((256,1,B),dtype=np.uint32)
 
     for g in range(256):
         hist[g, 0, ...] = np.sum(np.sum(i == g, 0), 0)
@@ -19,8 +19,8 @@ def probdensityfunc(histogram, shape):
     return probdensity
 
 
-histogram1 = np.zeros((256, 1, 3), dtype=np.uint8)
-histogram2 = np.zeros((256, 1, 3), dtype=np.uint8)
+histogram1 = np.zeros((256, 1, 3), dtype=np.uint32)
+histogram2 = np.zeros((256, 1, 3), dtype=np.uint32)
 probDensity1 = np.zeros((256, 1, 3), dtype=np.float32)
 probDensity2 = np.zeros((256, 1, 3), dtype=np.float32)
 cdf = np.zeros((256,1,1),dtype=np.uint8)
@@ -29,6 +29,7 @@ x_pos = [i for i in range(256)]
 
 img = cv2.imread("color1.png", cv2.IMREAD_COLOR)
 img2 = cv2.imread("color2.png", cv2.IMREAD_COLOR)
+print(np.sum(img,1))
 imgShape = img.shape[0] * img.shape[1]
 
 histogram1 = histogram(img)
@@ -38,8 +39,23 @@ probDensity1 = probdensityfunc(histogram1, imgShape)
 probDensity2 = probdensityfunc(histogram2, imgShape)
 
 
-print(probDensity1)
-print(probDensity2)
+
+print(histogram1[...,0,1])
+print("AAAAAAAAAAAAAA")
+print(histr)
+
+#prodens = histr/imgShape
+#print("me")
+#print(probDensity1[...,0,1])
+#print("you")
+#print(prodens)
+#print("me")
+#print(np.cumsum(probDensity1[...,0,1]))
+#print("you")
+#print(np.cumsum(prodens))
+#print(probDensity1)
+#print(probDensity2)
+
 
 plt.bar(x_pos, histogram1[..., 0, 1])
 plt.title("hist")
