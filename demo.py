7778318f7eb88693ae905dea7ecfff536d113ec9
berk <bdehrioglu@yahoo.com>
2018-10-21 16:59:42 +0300

    feat: handlers of open target and input

--- demo.py
+++ demo.py
@@ -7,7 +7,11 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import random
+import cv2
+import numpy as np
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -17,19 +21,55 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName("centralwidget")
 
         self.frame = QtWidgets.QFrame(self.centralwidget)
-        self.frame.setGeometry(QtCore.QRect(360, 50, 301, 601))
+        self.frame.setGeometry(QtCore.QRect(540, 50, 501, 1101))
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame.setObjectName("frame")
 
+        self.figure4 = Figure()
+        self.figure4.clear()
+        self.canvas4 = FigureCanvas(self.figure4)
+        self.canvas4.setParent(self.centralwidget)
+        self.canvas4.setGeometry(QtCore.QRect(540, 500, 501,200))
+
+        self.figure5 = Figure()
+        self.figure5.clear()
+        self.canvas5 = FigureCanvas(self.figure5)
+        self.canvas5.setParent(self.centralwidget)
+        self.canvas5.setGeometry(QtCore.QRect(540, 700, 501,200))
+
+        self.figure6 = Figure()
+        self.figure6.clear()
+        self.canvas6 = FigureCanvas(self.figure6)
+        self.canvas6.setParent(self.centralwidget)
+        self.canvas6.setGeometry(QtCore.QRect(540, 900, 501,200))
+
         self.frame_2 = QtWidgets.QFrame(self.centralwidget)
-        self.frame_2.setGeometry(QtCore.QRect(40, 50, 301, 601))
+        self.frame_2.setGeometry(QtCore.QRect(40, 50, 501, 1101))
         self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame_2.setObjectName("frame_2")
 
+        self.figure = Figure()
+        self.figure.clear()
+        self.canvas = FigureCanvas(self.figure)
+        self.canvas.setParent(self.centralwidget)
+        self.canvas.setGeometry(QtCore.QRect(40, 500, 501,200))
+
+        self.figure2 = Figure()
+        self.figure2.clear()
+        self.canvas2 = FigureCanvas(self.figure2)
+        self.canvas2.setParent(self.centralwidget)
+        self.canvas2.setGeometry(QtCore.QRect(40, 700, 501,200))
+
+        self.figure3 = Figure()
+        self.figure3.clear()
+        self.canvas3 = FigureCanvas(self.figure3)
+        self.canvas3.setParent(self.centralwidget)
+        self.canvas3.setGeometry(QtCore.QRect(40, 900, 501,200))
+
         self.frame_3 = QtWidgets.QFrame(self.centralwidget)
-        self.frame_3.setGeometry(QtCore.QRect(680, 50, 301, 601))
+        self.frame_3.setGeometry(QtCore.QRect(1041, 50, 501, 1101))
         self.frame_3.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame_3.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame_3.setObjectName("frame_3")
@@ -37,6 +77,7 @@ class Ui_MainWindow(object):
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(10, 0, 151, 32))
         self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.handleplot)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
@@ -62,6 +103,7 @@ class Ui_MainWindow(object):
         self.menubar.addAction(self.menuFile.menuAction())
         self.retranslateUi(MainWindow)
 
+
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
@@ -76,12 +118,84 @@ class Ui_MainWindow(object):
         picinput = QtWidgets.QLabel(self.frame_2)
         picinput.setPixmap(QtGui.QPixmap("color2.png"))
         picinput.show()
+        self.img = cv2.imread("color2.png")
+        self.x_pos = [i for i in range(256)]
+
+        self.histograminput = self.histogram(self.img)
+
+        self.axes = self.figure.add_subplot(111)
+        self.axes.bar(self.x_pos, self.histograminput[..., 0, 0], color='b')
+        self.canvas.draw()
+
+        self.axes2 = self.figure2.add_subplot(111)
+        self.axes2.bar(self.x_pos, self.histograminput[..., 0, 1], color='g')
+        self.canvas2.draw()
+
+        self.axes3 = self.figure3.add_subplot(111)
+        self.axes3.bar(self.x_pos, self.histograminput[..., 0, 2], color='r')
+        self.canvas3.draw()
         print("click")
 
     def handleopentarget(self):
         pictarget = QtWidgets.QLabel(self.frame)
         pictarget.setPixmap(QtGui.QPixmap("color1.png"))
         pictarget.show()
+        self.img2 = cv2.imread("color1.png")
+        self.x_pos2 = [i for i in range(256)]
+
+        self.histogramtarget = self.histogram(self.img2)
+
+        self.axes4 = self.figure4.add_subplot(111)
+        self.axes4.bar(self.x_pos2, self.histogramtarget[..., 0, 0], color='b')
+        self.canvas4.draw()
+
+        self.axes5 = self.figure5.add_subplot(111)
+        self.axes5.bar(self.x_pos2, self.histogramtarget[..., 0, 1], color='g')
+        self.canvas5.draw()
+
+        self.axes6 = self.figure6.add_subplot(111)
+        self.axes6.bar(self.x_pos2, self.histogramtarget[..., 0, 2], color='r')
+        self.canvas6.draw()
+
+
+    def histogram(self,i):
+        R, C, B = i.shape
+        hist = np.zeros((256, 1, B), dtype=np.uint32)
+
+        for g in range(256):
+            hist[g, 0, ...] = np.sum(np.sum(i == g, 0), 0)
+
+        return hist
+
+
+    def probdensityfunc(self,histogram, shape):
+        probdensity = np.zeros((256, 1, 3), dtype=np.float32)
+        probdensity = histogram / shape
+        return probdensity
+
+
+    def cumulativedistfuncrgb(self,probdensity):
+        return np.cumsum(probdensity[..., 0, 0]), np.cumsum(probdensity[..., 0, 1]), np.cumsum(probdensity[..., 0, 2])
+
+
+    def createlookuptable(self,cdfinput, cdftarget):
+        lut = np.zeros((256, 1, 3), dtype=np.float32)
+        gj = np.zeros(3, dtype=np.uint32)
+        for gi in range(256):
+            while (cdftarget[gj[0], 0, 0] < cdfinput[gi, 0, 0]) and (gj[0] < 255):
+                gj[0] = gj[0] + 1
+            lut[gi, 0, 0] = gj[0]
+
+            while (cdftarget[gj[1], 0, 1] < cdfinput[gi, 0, 1]) and (gj[1] < 255):
+                gj[1] = gj[1] + 1
+            lut[gi, 0, 1] = gj[0]
+
+            while (cdftarget[gj[2], 0, 2] < cdfinput[gi, 0, 2]) and (gj[2] < 255):
+                gj[2] = gj[2] + 1
+            lut[gi, 0, 2] = gj[2]
+
+        return lut
+
 
 if __name__ == "__main__":
     import sys
