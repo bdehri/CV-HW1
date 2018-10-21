193c16ac4c83683934dacc0506e0286731ee79aa
berk <bdehrioglu@yahoo.com>
2018-10-21 11:03:37 +0300

    feat: Action Bars button handlers added

--- demo.py
+++ demo.py
@@ -8,30 +8,36 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(949, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
         self.frame = QtWidgets.QFrame(self.centralwidget)
-        self.frame.setGeometry(QtCore.QRect(330, 50, 261, 501))
+        self.frame.setGeometry(QtCore.QRect(360, 50, 301, 601))
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame.setObjectName("frame")
+
         self.frame_2 = QtWidgets.QFrame(self.centralwidget)
-        self.frame_2.setGeometry(QtCore.QRect(40, 50, 261, 501))
+        self.frame_2.setGeometry(QtCore.QRect(40, 50, 301, 601))
         self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame_2.setObjectName("frame_2")
+
         self.frame_3 = QtWidgets.QFrame(self.centralwidget)
-        self.frame_3.setGeometry(QtCore.QRect(620, 50, 251, 501))
+        self.frame_3.setGeometry(QtCore.QRect(680, 50, 301, 601))
         self.frame_3.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame_3.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame_3.setObjectName("frame_3")
+
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(10, 0, 151, 32))
         self.pushButton.setObjectName("pushButton")
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 949, 22))
@@ -42,15 +48,20 @@ class Ui_MainWindow(object):
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+
+
         self.actionOpen_Input = QtWidgets.QAction(MainWindow)
         self.actionOpen_Input.setObjectName("actionOpen_Input")
+        self.actionOpen_Input.triggered.connect(self.handleopeninput)
+
         self.actionOpen_Target = QtWidgets.QAction(MainWindow)
         self.actionOpen_Target.setObjectName("actionOpen_Target")
+        self.actionOpen_Target.triggered.connect(self.handleopentarget)
         self.menuFile.addAction(self.actionOpen_Input)
         self.menuFile.addAction(self.actionOpen_Target)
         self.menubar.addAction(self.menuFile.menuAction())
-
         self.retranslateUi(MainWindow)
+
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
@@ -61,6 +72,16 @@ class Ui_MainWindow(object):
         self.actionOpen_Input.setText(_translate("MainWindow", "Open Input"))
         self.actionOpen_Target.setText(_translate("MainWindow", "Open Target"))
 
+    def handleopeninput(self):
+        picinput = QtWidgets.QLabel(self.frame_2)
+        picinput.setPixmap(QtGui.QPixmap("color2.png"))
+        picinput.show()
+        print("click")
+
+    def handleopentarget(self):
+        pictarget = QtWidgets.QLabel(self.frame)
+        pictarget.setPixmap(QtGui.QPixmap("color1.png"))
+        pictarget.show()
 
 if __name__ == "__main__":
     import sys
